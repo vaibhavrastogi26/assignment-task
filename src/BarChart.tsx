8fbf8ac9cb2f5d90302a08a7877cbd4f392c9636
vaibhavrastogi26 <rastogivaibhav99@gmail.com>
2023-04-18 15:26:02 +0530

    Reduced time complexity and added changes to make code more readble

--- src/BarChart.tsx
+++ src/BarChart.tsx
@@ -2,15 +2,17 @@ import ReactECharts from 'echarts-for-react';
 import data from './Wine-Data.json'
 
 const BarChart = () => {
-  // Filter out the alcohol categories
-  const alcoholCategories = data.map(ele => ele['Alcohol']).filter((item, pos, self) => self.indexOf(item) === pos)
-  // Find the minimum Magnesium value for each alcohol category
-  const minimumMagnesium = alcoholCategories.map(item => data.filter(ele => ele['Alcohol'] === item).map(ele => ele['Magnesium'])).map(item => Math.min(...item))
+  // Filter out the alcohol categories (unique category using Set())
+  const alcoholCategories = [...new Set(data.map(item => item['Alcohol']))]
+  // Find the minimum Magnesium value for each alcohol category (Reduced time complexity from O(n^2) to O(n))
+  const arr: any = alcoholCategories.map(item => ([])) //Declared an empty array of arrays with length of 'alcoholCategories'
+  data.map(item => arr[item['Alcohol'] - 1].push(item['Magnesium'])) //Added values of magnesium in the empty arrays category-wise
+  const minimumMagnesium = arr.map((item: any) => Math.min(...item))
   const options = {
     title: {
       text: 'Bar Chart - Minimum Magnesium Value corresponding to each Alcohol Category '
     },
-    xAxis : { 
+    xAxis: {
       type: 'category',
       data: alcoholCategories,
       name: 'Alcohol Category'
